f0d73fd7e79dd0eae79bea5f2e925c8f14402e2f
Pranay-Vaka <vakapranay05@gmail.com>
2023-11-18 23:26:23 +0000

    colourscheme stuff

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -5,22 +5,37 @@ vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
+
   use 'wbthomason/packer.nvim'
+  use 'm4xshen/autoclose.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  --require'nvim-tree/nvim-web-devicons'.setup()
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
+
 
   use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  'nvim-telescope/telescope.nvim',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
-  [[
+  use "rebelot/kanagawa.nvim"
+  use "lunarvim/Onedarker.nvim"
   use({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
 	  config = function()
-		  vim.cmd('colorscheme slate')
+		  vim.cmd('colorscheme kanagawa')
 	  end
   })
-  ]]
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
   use({
       "folke/trouble.nvim",
       config = function()
@@ -44,7 +59,7 @@ return require('packer').startup(function(use)
   use("theprimeagen/refactoring.nvim")
   use("mbbill/undotree")
   use("tpope/vim-fugitive")
-  use("nvim-treesitter/nvim-treesitter-context");
+  --use("nvim-treesitter/nvim-treesitter-context");
 
   use {
 	  'VonHeikemen/lsp-zero.nvim',
--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -1,4 +1,4 @@
-#vim.opt.guicursor = ""
+--vim.opt.guicursor = ""
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -7,21 +7,26 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.clipboard:append("unnamedplus")
+
 vim.opt.smartindent = true
---vim.opt.colorscheme = slate
 
 vim.opt.wrap = false
+vim.cmd[[set noshowmode
+set noruler
+set laststatus=0
+set noshowcmd]]
 
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+vim.cmd[[set clipboard=unnamedplus]]
+
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
+--vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
@@ -30,4 +35,6 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 --vim.opt.colorcolumn = "80"
-vim.cmd [[colorscheme slate]]
+vim.cmd[[autocmd BufReadPost,FileReadPost,BufNewFile,BufEnter * call system("tmux rename-window '" . expand("%:t") . "'")
+autocmd VimLeave * call system("tmux setw automatic-rename")
+]]
